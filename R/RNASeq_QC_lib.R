5cfc79d80dd7e834a926ba91ca36fd3291400a60
Joseph Tran <Joseph.Tran@versailles.inra.fr>
2015-02-10 16:01:40 +0100

    fix input file and comment debug logger

--- R/RNASeq_QC_lib.R
+++ R/RNASeq_QC_lib.R
@@ -263,12 +263,12 @@ isStatsDataFormat <- function(stats.df)
 loadDesignData <- function(file)
 {
   # load Design data
-  warn_err <- tryCatch.W.E(read.table(opt$design, header=TRUE, sep="\t", check.names=FALSE))
+  warn_err <- tryCatch.W.E(read.table(file, header=TRUE, sep="\t", check.names=FALSE))
   
   if (is.null(warn_err$warning) && is.null(warn_err$value$message))
   {
     design.df <- warn_err$value
-    debug(logger, paste("input design data dimensions: ", dim(design.df)[1], " x ", dim(design.df)[2], sep=""))
+#     debug(logger, paste("input design data dimensions: ", dim(design.df)[1], " x ", dim(design.df)[2], sep=""))
   } else {
     stop(paste(geterrmessage(), str(warn_err)))
   }
